0745f99c01bddae8bab72c58f4b7bfd5ebc1980d
Keval <skeval904@gmail.com>
2020-01-27 20:41:11 +0530

    Update Matrix_3.java

--- 3/src/Matrix_3.java
+++ 3/src/Matrix_3.java
@@ -4,45 +4,29 @@ public class Matrix_3 {
     public static void main(String[] args) {
         System.out.println("Keval Shah\nEnrollment no : 180410107105");
         Random g = new Random();
-        int[][] x = new int [7][7];
-        int i,j,k=0,l=0;
+        int[][] x = new int [6][7];
+        int i;
+        int j;
+        int k=0;
+        int l=0;
         for(i=0;i<6;i++){
+            int y=0;
             for(j=0;j<6;j++){
                 x[i][j]=g.nextInt(2);
-                System.out.print(x[i][j]);
-                if(j!=5){
-                    System.out.print(" | ");
+                System.out.print(x[i][j]+"   ");
+                if(x[i][j] == 1){
+                    y++;
                 }
+            }
+            x[i][6]=y;
+            System.out.print("   : "+y);
+            if(y%2==1){
+                System.out.print("  Odd");
+            }
+            else{
+                System.out.print("  Even");
             }
             System.out.print("\n");
-        }
-        for(i=0;i<6;i++){
-            k=0;
-            for(j=0;j<6;j++){
-                if(x[i][j]==1){
-                    k++;
-                }
-            }if(k%2!=0){x[i][6]=1;}
-        }
-        for(j=0;j<6;j++){
-            l=0;
-            for(i=0;i<6;i++){
-                if(x[i][j]==1){
-                    l++;
-                }
-            }if(l%2!=0){x[6][j]=1;}
-        }
-        System.out.println("Rows with odd 1's are:");
-        for(i=0;i<6;i++){
-            if(x[i][6]==1){
-                System.out.print((i+1)+", ");
-            }
-        }
-        System.out.println("\nColumns with odd 1's are:");
-        for(i=0;i<6;i++) {
-            if (x[6][i] == 1) {
-                System.out.print((i+1)+", ");
-            }
         }
     }
 }
